04f759373f277ca16d4fe5cfb84ce05f00ef9a49
jaker <jaker@10.144.107.127>
2019-03-07 12:54:07 -0800

    Fixed db connection and added correct javafx start

--- src/nflplayers/NFLPlayers.java
+++ src/nflplayers/NFLPlayers.java
@@ -14,29 +14,22 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.sql.*;
 
-class PlayWithMySQL {
 
-static Connection connect;
 
 /**
  *
  * @author jaker
  */
 public class NFLPlayers extends Application {
+
+    static Connection connect;
     
     @Override
     public void start(Stage primaryStage) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connect = DriverManager.getConnection("jdbc:mysql://ambari-head.csc.calpoly.edu:3306?zeroDateTimeBehavior=convertToNull [group15 on Default schema]");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+ 
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
@@ -45,18 +38,20 @@ public class NFLPlayers extends Application {
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
+
+ Scene scene = new Scene(root, 300, 250);
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://ambari-head.csc.calpoly.edu/group15","group15", "group15");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         launch(args);
     }
     
